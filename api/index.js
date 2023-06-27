3f3420c44b9661bdf2af7459b081893ffe11b2ab
Joel Dupraz <joel_dupraz@hotmail.com>
2023-06-27 16:43:26 -0300

    deploy url's modified

--- api/index.js
+++ api/index.js
@@ -8,9 +8,10 @@ const { PORT } = process.env || 3001;
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
   server.listen(3001, () => {
-    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
+    console.log(`%s listening at ${PORT}`);
+    temperamentInBd(); // eslint-disable-line no-console
   });
 });
 
 
-temperamentInBd();
+
